85a66b60065a0663486b54adc06ab4fe7c0cdd3d
ishubtripathi <ishubtripathi@gmail.com>
2024-10-10 17:21:13 +0530

    final publish

--- setup.py
+++ setup.py
@@ -13,10 +13,13 @@ setup(
     long_description=open('README.md').read(), 
     long_description_content_type='text/markdown', 
     url='https://github.com/ishubtripathi/Ingrab.git', 
+    keywords=['ingrab','InGrabe','instagram','insta','post','reel','downloader','cli'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License', 
-        'Operating System :: OS Independent',
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
     ],
     python_requires='>=3.6', 
 )
